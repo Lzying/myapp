3cf469e4e27fe842c98888eaf560509a8e20132f
Wing Liang <wing.liang@agileact.com>
2017-12-18 13:31:05 +0800

    修改登录界面

--- src/page/login/login.module.ts
+++ src/page/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // 路由模块。导进来就可以识别项目里面的所有路由路径，
+import { FormsModule }   from '@angular/forms';
 
 import { LoginComponent } from './login.component';
 
@@ -11,7 +12,8 @@ import { LoginComponent } from './login.component';
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ],
     providers: [],
 })
